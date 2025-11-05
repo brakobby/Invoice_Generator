dbeeed78129086fbfa564fb9da6b0665c9e8253c
brakobby <brakobby454@gmail.com>
2025-11-05 18:29:47 +0000

    creating models for invoice

--- invGenerator/invoices/models.py
+++ invGenerator/invoices/models.py
@@ -2,4 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class 
+class CustomerData(models.Model):
+    customer_fullname = models.CharField(max_length=255)
+    customer_email = models.EmailField(unique=True)
+    customer_phone = models.CharField(max_length=255)
+    customer_address = models.CharField(max_length=500)
+    added_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.customer_fullname
+    
+class InvoiceData(models.Model):
+    pass
+
